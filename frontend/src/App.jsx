6a758ac7c784be24786347f5919f57f7cc32e411
ayush28yadav <70971263+ayush28yadav@users.noreply.github.com>
2023-12-24 14:05:03 +0530

    front end updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,48 @@
-import { useState } from 'react';
-import { useColorMode } from '@chakra-ui/react';
-import {Routes, Route} from 'react-router-dom';
+import { Box, Container } from "@chakra-ui/react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import UserPage from "../pages/UserPage";
+import PostPage from "../pages/PostPage";
+import Header from "../components/Header";
+import HomePage from "../pages/HomePage";
+import AuthPage from "../pages/AuthPage";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import UpdateProfilePage from "../pages/UpdateProfilePage";
+import CreatePost from "../components/CreatePost";
 
+import  SettingsPage  from "../pages/SettingsPage";
 function App() {
-  const {colorMode, toggleColorMode} = useColorMode();
+	const user = useRecoilValue(userAtom);
+	const { pathname } = useLocation();
+	return (
+		<Box position={"relative"} w='full'>
+			<Container maxW={pathname === "/" ? { base: "620px", md: "900px" } : "620px"}>
+				<Header />
+				<Routes>
+					<Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
+					<Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
+					<Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to='/auth' />} />
 
-  return (
-    <div>
-      <Header/>
-      <Routes>
-
-      </Routes>
-    </div>
-  );
+					<Route
+						path='/:username'
+						element={
+							user ? (
+								<>
+									<UserPage />
+									<CreatePost />
+								</>
+							) : (
+								<UserPage />
+							)
+						}
+					/>
+					<Route path='/:username/post/:pid' element={<PostPage />} />
+				
+					<Route path='/settings' element={user ? <SettingsPage /> : <Navigate to={"/auth"} />} />
+				</Routes>
+			</Container>
+		</Box>
+	);
 }
 
 export default App;
